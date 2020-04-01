547b47f1b570fb7b5446528f879e2c2d3b917920
1khtiyar <ixty.now@bk.ru>
2020-04-02 00:36:51 +0400

    Added control algorithms.
    Updated the interface. Added the main cotrol algorithms and methods of handling the input by using keyboard.

--- Game of 2048 Main Program/Game of 2048 Main Program/Form1.Designer.cs
+++ Game of 2048 Main Program/Game of 2048 Main Program/Form1.Designer.cs
@@ -24,14 +24,17 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.RestartButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
             this.panel1.Location = new System.Drawing.Point(13, 13);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(539, 539);
@@ -47,6 +50,19 @@
             this.panel2.Size = new System.Drawing.Size(539, 31);
             this.panel2.TabIndex = 1;
             // 
+            // RestartButton
+            // 
+            this.RestartButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.RestartButton.FlatAppearance.BorderSize = 0;
+            this.RestartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.RestartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RestartButton.Location = new System.Drawing.Point(0, 0);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(269, 31);
+            this.RestartButton.TabIndex = 0;
+            this.RestartButton.Text = "Restart";
+            this.RestartButton.UseVisualStyleBackColor = false;
+            // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
@@ -61,18 +77,23 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // RestartButton
+            // tableLayoutPanel1
             // 
-            this.RestartButton.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.RestartButton.FlatAppearance.BorderSize = 0;
-            this.RestartButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.RestartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.RestartButton.Location = new System.Drawing.Point(0, 0);
-            this.RestartButton.Name = "RestartButton";
-            this.RestartButton.Size = new System.Drawing.Size(269, 31);
-            this.RestartButton.TabIndex = 0;
-            this.RestartButton.Text = "Restart";
-            this.RestartButton.UseVisualStyleBackColor = false;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 532);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
             // Form1
             // 
@@ -86,6 +107,7 @@
             this.Name = "Form1";
             this.Text = "Game of 2048";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -97,6 +119,7 @@
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button RestartButton;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
 
--- Game of 2048 Main Program/Game of 2048 Main Program/Form1.cs
+++ Game of 2048 Main Program/Game of 2048 Main Program/Form1.cs
@@ -10,13 +10,204 @@ using System.Windows.Forms;
 
 namespace Game_of_2048_Main_Program
 {
+    delegate void ControlHandler(int[,] theGameBoard);
     public partial class Form1 : Form
     {
+
+        ControlHandler actionAfterKeyDown;
+
+        public static int[,] gameBoard = new int[4, 4];
         public Form1()
         {
             InitializeComponent();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Random rd = new Random();
+                gameBoard[rd.Next(0, 4), rd.Next(0, 4)] = i % 2 == 0 ? 2 : 4;
+            }
         }
 
+
+        //Control functionality:
+        public static void Down(int[,] gameBoard)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                bool enough = false;
+                startRechecking:
+                for (int i = 0, helper = 0; i < 4; i++, helper++)
+                {
+                    if (i < 3 && gameBoard[i + 1, j] == 0)
+                    {
+                        gameBoard[i + 1, j] = gameBoard[i, j];
+                        gameBoard[i, j] = 0;
+                    }
+                    if (i > 0 && gameBoard[i, j] == 0)
+                    {
+                        gameBoard[i, j] = gameBoard[i - 1, j];
+                        gameBoard[i - 1, j] = 0;
+                    }
+                    i = helper == 3 || helper == 7 ? 0 : i; //recheck 2 times
+                }
+                if (enough)
+                {
+                    continue;
+                }
+                for (int i = 3; i > 0; i--)
+                {
+                    if (gameBoard[i, j] == gameBoard[i - 1, j])
+                    {
+                        gameBoard[i, j] *= 2;
+                        gameBoard[i - 1, j] = 0;
+                    }
+                }
+                enough = true;
+                goto startRechecking;
+            }
+        }
+        public static void Up(int[,] gameBoard)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                bool enough = false;
+                startRechecking:
+                for (int i = 3, helper = 0; i >= 0; i--, helper++)
+                {
+                    if (i > 0 && gameBoard[i - 1, j] == 0)
+                    {
+                        gameBoard[i - 1, j] = gameBoard[i, j];
+                        gameBoard[i, j] = 0;
+                    }
+                    if (i < 3 && gameBoard[i, j] == 0)
+                    {
+                        gameBoard[i, j] = gameBoard[i + 1, j];
+                        gameBoard[i + 1, j] = 0;
+                    }
+                    i = helper == 3 || helper == 7 ? 0 : i; //recheck 2 times
+                }
+                if (enough)
+                {
+                    continue;
+                }
+                for (int i = 0; i < 3; i++)
+                {
+                    if (gameBoard[i, j] == gameBoard[i + 1, j])
+                    {
+                        gameBoard[i, j] *= 2;
+                        gameBoard[i + 1, j] = 0;
+                    }
+                }
+                enough = true;
+                goto startRechecking;
+            }
+        }
+        public static void Right(int[,] gameBoard)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                bool enough = false;
+                startRechecking:
+                for (int j = 0, helper = 0; j < 4; j++, helper++)
+                {
+                    if (j < 3 && gameBoard[i, j + 1] == 0)
+                    {
+                        gameBoard[i, j + 1] = gameBoard[i, j];
+                        gameBoard[i, j] = 0;
+                    }
+                    if (j > 0 && gameBoard[i, j] == 0)
+                    {
+                        gameBoard[i, j] = gameBoard[i, j - 1];
+                        gameBoard[i, j - 1] = 0;
+                    }
+                    j = helper == 3 || helper == 7 ? 0 : j; //recheck 2 times
+                }
+                if (enough)
+                {
+                    continue;
+                }
+                for (int j = 3; j > 0; j--)
+                {
+                    if (gameBoard[i, j] == gameBoard[i, j - 1])
+                    {
+                        gameBoard[i, j] *= 2;
+                        gameBoard[i, j - 1] = 0;
+                    }
+                }
+                enough = true;
+                goto startRechecking;
+            }
+        }
+        public static void Left(int[,] gameBoard)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                bool enough = false;
+                startRechecking:
+                for (int j = 3, helper = 0; j >= 0; j--, helper++)
+                {
+                    if (j > 0 && gameBoard[i, j - 1] == 0)
+                    {
+                        gameBoard[i, j - 1] = gameBoard[i, j];
+                        gameBoard[i, j] = 0;
+                    }
+                    if (j < 3 && gameBoard[i, j] == 0)
+                    {
+                        gameBoard[i, j] = gameBoard[i, j + 1];
+                        gameBoard[i, j + 1] = 0;
+                    }
+                    j = helper == 3 || helper == 7 ? 0 : j; //recheck 2 times
+                }
+                if (enough)
+                {
+                    continue;
+                }
+                for (int j = 0; j < 3; j++)
+                {
+                    if (gameBoard[i, j] == gameBoard[i, j + 1])
+                    {
+                        gameBoard[i, j] *= 2;
+                        gameBoard[i, j + 1] = 0;
+                    }
+                }
+                enough = true;
+                goto startRechecking;
+            }
+        }
+
+
+        //Control handlers:
+
+        private void ControlKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right) {
+                actionAfterKeyDown = Form1.Right;
+                actionAfterKeyDown(Form1.gameBoard);
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                actionAfterKeyDown = Form1.Left;
+                actionAfterKeyDown(Form1.gameBoard);
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                actionAfterKeyDown = Form1.Up;
+                actionAfterKeyDown(Form1.gameBoard);
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                actionAfterKeyDown = Form1.Down;
+                actionAfterKeyDown(Form1.gameBoard);
+            }
+
+        }
+
+
+
+        //public void PutResultsOnBoard()
+        //{
+        //    this.tableLayoutPanel1.ColumnStyles[]
+        //}
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -25,6 +216,12 @@ namespace Game_of_2048_Main_Program
         private void Button1_Click(object sender, EventArgs e)
         {
             this.Close();
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
